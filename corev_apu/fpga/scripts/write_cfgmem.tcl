1e139d57975bebf66987e362cfe56b2a1208ff1d
NightwalkeRhea <rastgooasma835@gmail.com>
2025-03-26 14:21:33 +0100

    generate mcs from bitstream

--- corev_apu/fpga/scripts/write_cfgmem.tcl
+++ corev_apu/fpga/scripts/write_cfgmem.tcl
@@ -34,6 +34,8 @@ if {$::env(BOARD) eq "genesys2"} {
     write_cfgmem -format mcs -interface SPIx4 -size 128  -loadbit "up 0x0 $bitfile" -file $mcsfile -force
 } elseif {$::env(BOARD) eq "nexys_video"} {
     write_cfgmem -format mcs -interface SPIx4 -size 256  -loadbit "up 0x0 $bitfile" -file $mcsfile -force
+} elseif {$::env(BOARD) eq "zcu102"} {
+    write_cfgmem -format mcs -interface qspi_x4 -size 512 -loadbit "up 0x0 $bitfile" -file $mcsfile -force
 } else {
       exit 1
 }
